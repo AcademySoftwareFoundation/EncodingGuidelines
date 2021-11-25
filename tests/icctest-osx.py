d0b6bfd6f79948356f17fd34e2c6929799ab4398
richardssam <sam.richards@disney.com>
2021-11-25 13:19:33 +0000

    Made it sorted a bit more sensibly and added filtering.

--- tests/icctest-osx.py
+++ tests/icctest-osx.py
@@ -23,37 +23,37 @@ img.save(source_image)
 #source_image = os.path.join(rootpath, "greyscale-srgb-photoshop.png")
 
 listimages = []
-listimages.append({'id': 'raw', 'label': 'raw png', 'image': os.path.basename(source_image)})
+listimages.append({'id': 'raw', 'label': 'raw ', 'image': os.path.basename(source_image), 'group': 'raw'})
 
 profile = ImageCms.getOpenProfile("/usr/share/color/icc/sRGB.icc")
 img.save(os.path.join(rootpath, "greyscale-srgb.png"), icc_profile=profile.tobytes())
-listimages.append({'id': 'srgbpng', 'label': 'srgb png', 'image': "greyscale-srgb.png", 'group': 'srgb'})
+listimages.append({'id': 'srgbpng', 'label': 'srgb', 'ext': 'png', 'image': "greyscale-srgb.png", 'group': 'srgb'})
 
 profile = ImageCms.getOpenProfile(r"../ICC/Simplified-sRGB.icc")
 img.save(os.path.join(rootpath, "greyscale-g22.png"), icc_profile=profile.tobytes())
-listimages.append({'id': 'g22png', 'label': 'gamma2.2 png', 'image': "greyscale-g22.png", 'group': 'gamma22'})
+listimages.append({'id': 'g22png', 'label': 'gamma2.2', 'ext': 'png', 'image': "greyscale-g22.png", 'group': 'gamma22'})
 
 profile = ImageCms.getOpenProfile(r"../ICC/Rec709-Rec1886.icc")
 img.save(os.path.join(rootpath, "greyscale-rec1886.png"), icc_profile=profile.tobytes())
-listimages.append({'id': 'rec1886png', 'label': 'rec1886 png', 'image': "greyscale-rec1886.png", 'group': 'bt1886'})
+listimages.append({'id': 'rec1886png', 'label': 'rec1886', 'ext': 'png', 'image': "greyscale-rec1886.png", 'group': 'bt1886'})
 
 # Now lets make the mp4's.
 os.system('ffmpeg -r 1 -y -i  ' + source_image + '  -sws_flags spline+accurate_rnd+full_chroma_int -vf "scale=in_range=full:in_color_matrix=bt709:out_range=tv:out_color_matrix=bt709" -c:v libx264  -pix_fmt yuv420p -qscale:v 1 ' + rootpath+'/greyscale-raw.mp4')
-listimages.append({'id': 'raw', 'label': 'raw', 'video': "greyscale-raw.mp4"})
+listimages.append({'id': 'raw', 'label': 'raw', 'ext': 'mp4', 'video': "greyscale-raw.mp4", 'group': 'raw'})
 
 trc_types = [{'id': 'rec709', 'label': "-color_trc 1 = rec709", 'fileext': "rec709", 'trcnum': 1, 'group': 'rec709'},
-            {'id': 'gamma195', 'label': "-color_trc 2 = gamma 1.95", 'fileext': "gamma195", 'trcnum': 2, 'gamma': 1.95, 'group': 'rec709'},
+            {'id': 'gamma195', 'label': "(OSX Only) -color_trc 2 = gamma 1.95", 'fileext': "gamma195", 'trcnum': 2, 'gamma': 1.95, 'group': 'rec709'},
 			{'id': 'unknown', 'label': "-color_trc 2 = unknown", 'fileext': "unknown", 'trcnum': 2},
 			{'id': 'srgb', 'label': "-color_trc 13 = sRGB", 'fileext': "srgb", 'trcnum': 13, 'group': 'srgb'},
 			#{'label': "-color_trc 14 = rec2020", 'fileext': "rec2020", 'trcnum': 14},
 			#{'label': "-color_trc 15 = rec2020", 'fileext': "rec2020b", 'trcnum': 15},
 			{'id': 'gamma22mp4', 'label': "-color_trc 4 = gamma 2.2", 'fileext': "gamma22", 'trcnum': 4, 'group': 'gamma22'},
-            {'id': 'gamma22mov', 'label': "-color_trc 2 = gamma 2.2", 'fileext': "gamma22", 'trcnum': 2, 'gamma': 2.2, 'group': 'gamma22'},
-            {'id': 'gamma24mov', 'label': "-color_trc 2 = gamma 2.4", 'fileext': "gamma24", 'trcnum': 2, 'gamma': 2.4, 'group': 'bt1886'},
+            {'id': 'gamma22mov', 'label': "(OSX Only) -color_trc 2 = gamma 2.2", 'fileext': "gamma22", 'trcnum': 2, 'gamma': 2.2, 'group': 'gamma22'},
+            {'id': 'gamma24mov', 'label': "(OSX Only) -color_trc 2 = gamma 2.4 BT1886", 'fileext': "gamma24", 'trcnum': 2, 'gamma': 2.4, 'group': 'bt1886'},
 			{'id': 'gamma28mp4', 'label': "-color_trc 5 = gamma 2.8", 'fileext': "gamma28", 'trcnum': 5, 'group': 'gamma28'},
-            {'id': 'gamma28mov', 'label': "-color_trc 2 = gamma 2.8", 'fileext': "gamma28", 'trcnum': 2, 'gamma': 2.8, 'group': 'gamma28'},
+            {'id': 'gamma28mov', 'label': "(OSX Only) -color_trc 2 = gamma 2.8", 'fileext': "gamma28", 'trcnum': 2, 'gamma': 2.8, 'group': 'gamma28'},
 			{'id': 'gammalinmp4', 'label': "-color_trc 8 = linear", 'fileext': "lin", 'trcnum': 8, 'group': 'lin'},
-            {'id': 'gammalinmov', 'label': "-color_trc 2 = gamma 1", 'fileext': "gamma1", 'trcnum': 2, 'gamma': 1, 'group': 'lin'},
+            {'id': 'gammalinmov', 'label': "(OSX Only) -color_trc 2 = gamma 1", 'fileext': "gamma1", 'trcnum': 2, 'gamma': 1, 'group': 'lin'},
 			]
 for trc in trc_types:
 	# TODO Confirm we have the right one.
@@ -68,10 +68,28 @@ for trc in trc_types:
 	trc['ext'] = ext
 	listimages.append({'id': trc['id'], 'group': trc.get('group', 'unknown'), 'label': trc['label'], 'video': "greyscale-{fileext}.{ext}".format(**trc), 'cmd': cmd})
 
+# Sort images by group.
+listimages = sorted(listimages, key=lambda k:k['group'])
 
+introduction = """
+<H1>Color_trc comparison for OSX</H1>
+<p> This is comparing a png file written with different ICC profiles (i.e. the underlying data is identical in all png files), and comparing it to mp4's where the only change is the -color_trc flag setting, along with mov files with different gamma values. The code to generate these files is <a href='../%s'>here</a>. You can reorder the wedges to help with comparison by drag and drop.</p>
+Filtered views:
+<p>
+<bl>
+<li><A href="compare.html?.groupbt1886">bt1886</a></li>
+<li><A href="compare.html?.groupsrgb">sRGB</a></li>
+<li><A href="compare.html?.groupgamma22">gamma 2.2</a></li>
+<li><A href="compare.html?.groupgamma22+.groupsrgb">gamma 2.2 and sRGB</a></li>
+<li><A href="compare.html?.groupgamma28">gamma 2.8</a></li>
+<li><A href="compare.html?.grouplin">linear</a></li>
+
+</bl>
+</p>
+""" % os.path.basename(__file__)
 
 createCompareHtml(outputpath=rootpath+"/compare.html", 
 					listimages=listimages,
-					introduction="<H1>Color_trc comparison for OSX</H1><p> This is comparing a png file written with different ICC profiles (i.e. the underlying data is identical in all png files), and comparing it to mp4's where the only change is the -color_trc flag setting, along with mov files with different gamma values. The code to generate these files is <a href='../%s'>here</a>. You can reorder the wedges to help with comparison by drag and drop.</p>" % os.path.basename(__file__),
+					introduction=introduction,
                                     videohtml = '  '
                                         )
