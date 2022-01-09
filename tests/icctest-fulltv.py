75aa61b3713403a4689a8e3ad29ea2f2cf7e741d
richardssam <sam.richards@disney.com>
2022-01-09 08:59:45 -0800

    Adding back the grey-ramp, as well as a 444 version.

--- tests/icctest-fulltv.py
+++ tests/icctest-fulltv.py
@@ -21,47 +21,62 @@ for icol in range(0, 256):
 source_image = os.path.join(rootpath, "greyscale-raw.png")
 img.save(source_image)
 #source_image = os.path.join(rootpath, "greyscale-srgb-photoshop.png")
-source_image = "../sourceimages/radialgrad.png"
 listimages = []
-listimages.append({'id': 'greypng', 'label': 'raw png', 'image': os.path.join("..", source_image)})
+listimages.append({'id': 'greypng', 'label': 'raw greyramp png', 'image': os.path.join("..", source_image)})
 # Now lets make the mp4's.
-
-cmd = 'ffmpeg -y -i  {source_image}  -sws_flags spline+accurate_rnd+full_chroma_int -vf "scale=in_range=full:in_color_matrix=bt709:out_range={outrange}:out_color_matrix=bt709" -c:v libx264  -pix_fmt yuv420p -qscale:v 1  -color_range 1 -colorspace 1 -color_primaries 1 -color_trc 1 {rootpath}/greyscale-{fileext}.mp4'.format(outrange="tv", source_image=source_image, fileext="tv", rootpath=rootpath)
+cmd = 'ffmpeg -y -loop 1 -i  {source_image}  -sws_flags spline+accurate_rnd+full_chroma_int -vf "scale=in_range=full:in_color_matrix=bt709:out_range={outrange}:out_color_matrix=bt709" -c:v libx264  -t 5 -pix_fmt yuv420p -qscale:v 1  -color_range 1 -colorspace 1 -color_primaries 1 -color_trc 1 {rootpath}/greyscale-{fileext}.mp4'.format(outrange="tv", source_image=source_image, fileext="tv", rootpath=rootpath)
 os.system(cmd)
-listimages.append({'id': 'greytv', 'label': 'out_range=tv -pix_fmt yuv420p 16-235 range', 'video': "greyscale-tv.mp4", 'cmd': cmd})
+listimages.append({'id': 'greytv', 'label': 'greyramp out_range=tv -pix_fmt yuv420p 16-235 range', 'video': "greyscale-tv.mp4", 'cmd': cmd})
 
-cmd = 'ffmpeg -y -i  {source_image}  -sws_flags spline+accurate_rnd+full_chroma_int -vf "scale=in_range=full:in_color_matrix=bt709:out_range={outrange}:out_color_matrix=bt709" -c:v libx264  -pix_fmt yuvj420p -qscale:v 1  -color_range 2 -colorspace 1 -color_primaries 1 -color_trc 1 {rootpath}/greyscale-{fileext}.mp4'.format(outrange="full", source_image=source_image, fileext="fullj", rootpath=rootpath)
+cmd = 'ffmpeg -y -loop 1 -i  {source_image}  -sws_flags spline+accurate_rnd+full_chroma_int -vf "scale=in_range=full:in_color_matrix=bt709:out_range={outrange}:out_color_matrix=bt709" -c:v libx264  -t 5 -pix_fmt yuv420p -qscale:v 1  -color_range 2 -colorspace 1 -color_primaries 1 -color_trc 1 {rootpath}/greyscale-{fileext}.mp4'.format(outrange="full", source_image=source_image, fileext="full", rootpath=rootpath)
 os.system(cmd)
-listimages.append({'id': 'greyfullj', 'label': 'out_range=full yuvj420p 0-255 range', 'video': "greyscale-fullj.mp4", 'cmd': cmd})
+listimages.append({'id': 'greyfull', 'label': 'greyramp out_range=full -pix_fmt yuv420p color_range=2', 'video': "greyscale-full.mp4", 'cmd': cmd})
 
-cmd = 'ffmpeg -y -i  {source_image}  -sws_flags spline+accurate_rnd+full_chroma_int -vf "scale=in_range=full:in_color_matrix=bt709:out_range={outrange}:out_color_matrix=bt709" -c:v libx264  -pix_fmt yuv420p -qscale:v 1  -color_range 2 -colorspace 1 -color_primaries 1 -color_trc 1 {rootpath}/greyscale-{fileext}.mp4'.format(outrange="full", source_image=source_image, fileext="full", rootpath=rootpath)
+source_image = "../sourceimages/radialgrad.png"
+listimages.append({'id': 'radialgradpng', 'label': 'raw png', 'image': os.path.join("..", source_image)})
+
+
+cmd = 'ffmpeg -y -loop 1 -i  {source_image}  -sws_flags spline+accurate_rnd+full_chroma_int -vf "scale=in_range=full:in_color_matrix=bt709:out_range={outrange}:out_color_matrix=bt709" -c:v libx264  -t 5 -pix_fmt yuv420p -qscale:v 1  -color_range 1 -colorspace 1 -color_primaries 1 -color_trc 1 {rootpath}/radialgrad-{fileext}.mp4'.format(outrange="tv", source_image=source_image, fileext="tv", rootpath=rootpath)
 os.system(cmd)
-listimages.append({'id': 'greyfull', 'label': 'out_range=full -pix_fmt yuv420p color_range=2', 'video': "greyscale-full.mp4", 'cmd': cmd})
+listimages.append({'id': 'radialgradtv', 'label': 'out_range=tv -pix_fmt yuv420p 16-235 range', 'video': "radialgrad-tv.mp4", 'cmd': cmd})
 
-
-cmd = 'ffmpeg -y -i  {source_image}  -sws_flags spline+accurate_rnd+full_chroma_int -vf "scale=in_range=full:in_color_matrix=bt709:out_range={outrange}:out_color_matrix=bt709" -c:v libx264  -pix_fmt yuv420p -qscale:v 1  -color_range 0 -colorspace 1 -color_primaries 1 -color_trc 1 {rootpath}/greyscale0-{fileext}.mp4'.format(outrange="full", source_image=source_image, fileext="full", rootpath=rootpath)
+cmd = 'ffmpeg -y -loop 1 -i  {source_image}  -sws_flags spline+accurate_rnd+full_chroma_int -vf "scale=in_range=full:in_color_matrix=bt709:out_range={outrange}:out_color_matrix=bt709" -c:v libx264  -t 5 -pix_fmt yuvj420p -qscale:v 1  -color_range 2 -colorspace 1 -color_primaries 1 -color_trc 1 {rootpath}/radialgrad-{fileext}.mp4'.format(outrange="full", source_image=source_image, fileext="fullj", rootpath=rootpath)
 os.system(cmd)
-listimages.append({'id': 'greyfull0', 'label': 'out_range=full -pix_fmt yuv420p color_range=0', 'video': "greyscale0-full.mp4", 'cmd': cmd})
+listimages.append({'id': 'radialgradfullj', 'label': 'out_range=full yuvj420p 0-255 range', 'video': "radialgrad-fullj.mp4", 'cmd': cmd})
 
-cmd = 'ffmpeg -y -i {source_image}  -c:v libx264 -preset placebo -qp 0 -x264-params "keyint=15:no-deblock=1" -pix_fmt yuv444p10le -sws_flags spline+accurate_rnd+full_chroma_int -vf "colorspace=bt709:iall=bt601-6-625:fast=1" {rootpath}/greyscale-raw-10bit.mp4'.format(source_image=source_image, rootpath=rootpath)
+cmd = 'ffmpeg -y -loop 1 -i  {source_image}  -sws_flags spline+accurate_rnd+full_chroma_int -vf "scale=in_range=full:in_color_matrix=bt709:out_range={outrange}:out_color_matrix=bt709" -c:v libx264  -t 5 -pix_fmt yuv420p -qscale:v 1  -color_range 2 -colorspace 1 -color_primaries 1 -color_trc 1 {rootpath}/radialgrad-{fileext}.mp4'.format(outrange="full", source_image=source_image, fileext="full", rootpath=rootpath)
 os.system(cmd)
-listimages.append({'id': 'greytv10', 'label': 'yuv444p10le 10-bit mp4', 'video': "greyscale-raw-10bit.mp4", 'cmd': cmd})
+listimages.append({'id': 'radialgradfull', 'label': 'out_range=full -pix_fmt yuv420p color_range=2', 'video': "radialgrad-full.mp4", 'cmd': cmd})
 
-cmd = 'ffmpeg -y -i {source_image}  -c:v libx264rgb -preset placebo -qp 0 -x264-params "keyint=15:no-deblock=1" {rootpath}/greyscale-raw-rgb.mp4'.format(source_image=source_image, rootpath=rootpath)
+
+cmd = 'ffmpeg -y -loop 1 -i  {source_image}  -sws_flags spline+accurate_rnd+full_chroma_int -vf "scale=in_range=full:in_color_matrix=bt709:out_range={outrange}:out_color_matrix=bt709" -c:v libx264  -t 5 -pix_fmt yuv420p -qscale:v 1  -color_range 0 -colorspace 1 -color_primaries 1 -color_trc 1 {rootpath}/radialgrad-{fileext}.mp4'.format(outrange="full", source_image=source_image, fileext="full0", rootpath=rootpath)
 os.system(cmd)
-listimages.append({'id': 'greyrgb', 'label': 'libx264rgb 8-bit mp4 (not supported)', 'video': "greyscale-raw-rgb.mp4", 'cmd': cmd})
+listimages.append({'id': 'radialgradfull0', 'label': 'out_range=full -pix_fmt yuv420p color_range=0', 'video': "radialgrad-full0.mp4", 'cmd': cmd})
+
+cmd = 'ffmpeg -y -loop 1 -i  {source_image}  -sws_flags spline+accurate_rnd+full_chroma_int -vf "scale=in_range=full:in_color_matrix=bt709:out_range={outrange}:out_color_matrix=bt709" -c:v libx264  -t 5 -pix_fmt yuv444p -qscale:v 1  -color_range 2 -colorspace 1 -color_primaries 1 -color_trc 1 {rootpath}/radialgrad-{fileext}.mp4'.format(outrange="full", source_image=source_image, fileext="full444", rootpath=rootpath)
+os.system(cmd)
+listimages.append({'id': 'radialgradfull444', 'label': 'out_range=full -pix_fmt yuv444p color_range=2', 'video': "radialgrad-full444.mp4", 'cmd': cmd})
+
+
+cmd = 'ffmpeg -y -loop 1 -i {source_image}  -c:v libx264 -t 5 -preset placebo -qp 0 -x264-params "keyint=15:no-deblock=1" -pix_fmt yuv444p10le -sws_flags spline+accurate_rnd+full_chroma_int -vf "colorspace=bt709:iall=bt601-6-625:fast=1" {rootpath}/radialgrad-raw-10bit.mp4'.format(source_image=source_image, rootpath=rootpath)
+os.system(cmd)
+listimages.append({'id': 'radialgradtv10', 'label': 'yuv444p10le 10-bit mp4', 'video': "radialgrad-raw-10bit.mp4", 'cmd': cmd})
+
+cmd = 'ffmpeg -y -loop 1 -i {source_image}  -c:v libx264rgb -t 5 -preset placebo -qp 0 -x264-params "keyint=15:no-deblock=1" {rootpath}/radialgrad-raw-rgb.mp4'.format(source_image=source_image, rootpath=rootpath)
+os.system(cmd)
+listimages.append({'id': 'radialgradrgb', 'label': 'libx264rgb 8-bit mp4 (not supported)', 'video': "radialgrad-raw-rgb.mp4", 'cmd': cmd})
 
 
 source_image = "../sourceimages/Digital_LAD_sRGB.png"
-cmd = 'ffmpeg -y -i  {source_image}  -sws_flags spline+accurate_rnd+full_chroma_int -vf "scale=in_range=full:in_color_matrix=bt709:out_range={outrange}:out_color_matrix=bt709" -c:v libx264  -pix_fmt yuv420p -qscale:v 1  -color_range 1 -colorspace 1 -color_primaries 1 -color_trc 1 {rootpath}/marcie-{fileext}.mp4'.format(outrange="tv", source_image=source_image, fileext="tv", rootpath=rootpath)
+cmd = 'ffmpeg -y -loop 1 -i  {source_image}  -sws_flags spline+accurate_rnd+full_chroma_int -vf "scale=in_range=full:in_color_matrix=bt709:out_range={outrange}:out_color_matrix=bt709" -c:v libx264  -t 5 -pix_fmt yuv420p -qscale:v 1  -color_range 1 -colorspace 1 -color_primaries 1 -color_trc 1 {rootpath}/marcie-{fileext}.mp4'.format(outrange="tv", source_image=source_image, fileext="tv", rootpath=rootpath)
 os.system(cmd)
 listimages.append({'id': 'marcietv', 'label': 'marcie out_range=tv -pix_fmt yuv420p 16-235 range', 'video': "marcie-tv.mp4", 'cmd': cmd})
 
-cmd = 'ffmpeg -y -i  {source_image}  -sws_flags spline+accurate_rnd+full_chroma_int -vf "scale=in_range=full:in_color_matrix=bt709:out_range={outrange}:out_color_matrix=bt709" -c:v libx264  -pix_fmt yuvj420p -qscale:v 1  -color_range 0 -colorspace 1 -color_primaries 1 -color_trc 1 {rootpath}/marcie-{fileext}.mp4'.format(outrange="full", source_image=source_image, fileext="fullj", rootpath=rootpath)
+cmd = 'ffmpeg -y -loop 1 -i  {source_image}  -sws_flags spline+accurate_rnd+full_chroma_int -vf "scale=in_range=full:in_color_matrix=bt709:out_range={outrange}:out_color_matrix=bt709" -c:v libx264  -t 5 -pix_fmt yuvj420p -qscale:v 1  -color_range 0 -colorspace 1 -color_primaries 1 -color_trc 1 {rootpath}/marcie-{fileext}.mp4'.format(outrange="full", source_image=source_image, fileext="fullj", rootpath=rootpath)
 os.system(cmd)
 listimages.append({'id': 'marciefullj', 'label': 'marcie out_range=full yuvj420p 0-255 range', 'video': "marcie-fullj.mp4", 'cmd': cmd})
 
-cmd = 'ffmpeg -y -i  {source_image}  -sws_flags spline+accurate_rnd+full_chroma_int -vf "scale=in_range=full:in_color_matrix=bt709:out_range={outrange}:out_color_matrix=bt709" -c:v libx264  -pix_fmt yuv420p -qscale:v 1  -color_range 2 -colorspace 1 -color_primaries 1 -color_trc 1 {rootpath}/marcie-{fileext}.mp4'.format(outrange="full", source_image=source_image, fileext="full", rootpath=rootpath)
+cmd = 'ffmpeg -y -loop 1 -i  {source_image}  -sws_flags spline+accurate_rnd+full_chroma_int -vf "scale=in_range=full:in_color_matrix=bt709:out_range={outrange}:out_color_matrix=bt709" -c:v libx264  -t 5 -pix_fmt yuv420p -qscale:v 1  -color_range 2 -colorspace 1 -color_primaries 1 -color_trc 1 {rootpath}/marcie-{fileext}.mp4'.format(outrange="full", source_image=source_image, fileext="full", rootpath=rootpath)
 os.system(cmd)
 listimages.append({'id': 'marciefull', 'label': 'marcie out_range=full -pix_fmt yuv420p -color_range 2', 'video': "marcie-full.mp4", 'cmd': cmd})
 
