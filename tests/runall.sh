4d6bd24e230fa212a2f82659af6215a790367369
richardssam <sam.richards@disney.com>
2022-04-30 17:05:10 -0700

    Remove some of the older tests.

--- tests/runall.sh
+++ tests/runall.sh
@@ -1,5 +1,7 @@
 python3 chip-color-test.py
 python3 icctest.py
 python3 icctest-fulltv.py
-python3 icctest-rev-ps-nuke.py
-python3 icctest-rev-ps.py
+# python3 icctest-rev-ps-nuke.py
+# python3 icctest-rev-ps.py
+python3 icctest-fulltv.py
+python3 icctest-osx.py
