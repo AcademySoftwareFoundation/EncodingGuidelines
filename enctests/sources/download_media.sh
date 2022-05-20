04181fab94a26e1e173fc4534975e039e2fa9197
Sam Richards <sam_junk@taurich.org>
2022-05-19 21:54:45 -0700

    Update download_media.sh

--- enctests/sources/download_media.sh
+++ enctests/sources/download_media.sh
@@ -7,3 +7,7 @@ convert -verbose -size 1920x1080 SMPTE_Color_Bars.svg SMPTE_Color_Bars.png
 #mkdir -p /usr/local/share/model
 #curl https://raw.githubusercontent.com/Netflix/vmaf/master/model/vmaf_v0.6.1.pkl.model -o /usr/local/share/model/vmaf_v0.6.1.pkl.model
 #curl https://raw.githubusercontent.com/Netflix/vmaf/master/model/vmaf_v0.6.1.pkl -o /usr/local/share/model/vmaf_v0.6.1.pkl
+
+#downloading sparks
+~/aws-cli/aws s3 cp --no-sign-request s3://download.opencontent.netflix.com/sparks/aces_image_sequence_59_94_fps/ sources --recursive --exclude "*" --include "SPARKS_ACES_06*.exr"
+https://opencontent.netflix.com/
