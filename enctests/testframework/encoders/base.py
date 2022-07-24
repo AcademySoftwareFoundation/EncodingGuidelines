12d4a207274536ebd112dd1df77a6fae8c70ab7d
apetrynet <flehnerheener@gmail.com>
2022-07-24 16:01:42 +0200

    cut down amount of required methods

--- enctests/testframework/encoders/base.py
+++ enctests/testframework/encoders/base.py
@@ -1,5 +1,4 @@
 import pathlib
-from typing import Tuple
 from abc import ABC, abstractmethod
 
 import opentimelineio as otio
@@ -27,18 +26,3 @@ class ABCTestEncoder(ABC):
     @abstractmethod
     def get_application_version(self) -> str:
         """Return version of encoder application"""
-
-    @abstractmethod
-    def prep_encoding_command(self, wedge: dict, out_file: pathlib.Path) -> str:
-        """Assemble the command passed to subprocess for encoding"""
-
-    @abstractmethod
-    def get_output_filename(self, test_name: str) -> pathlib.Path:
-        """Return a filename including test name suffix"""
-
-    @abstractmethod
-    def get_source_path(self) -> Tuple[pathlib.Path, str]:
-        """
-        Get a tuple containing path and frame abstraction based on
-        self.source_clip formatted to support encoder syntax
-        """
