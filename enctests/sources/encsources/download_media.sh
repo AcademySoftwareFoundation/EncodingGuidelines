ae2ba7c15989abfc7cf48dddc600484f3b18a8a8
richardssam <sam.richards@disney.com>
2022-09-09 19:11:51 -0700

    Splitting up the source-media downloads.

---
+++ enctests/sources/encsources/download_media.sh
@@ -0,0 +1,69 @@
+#!/usr/bin/bash -xv
+
+#downloading chimera
+
+# https://opencontent.netflix.com/
+# This first bit of media is really just for sparks, which hopefully should stress encoders.
+if [ ! -d chimera_wind_srgb ]
+then
+	aws s3 cp --no-sign-request s3://download.opencontent.netflix.com/Chimera/tif_DCI4k5994p/ chimera_wind --recursive --exclude "*" --include "*_790*.tif"
+	aws s3 cp --no-sign-request s3://download.opencontent.netflix.com/Chimera/tif_DCI4k5994p/ chimera_wind --recursive --exclude "*" --include "*_791*.tif"
+fi
+
+if [ ! -d chimera_coaster_srgb ]
+then
+	aws s3 cp --no-sign-request s3://download.opencontent.netflix.com/Chimera/tif_DCI4k5994p/ chimera_coaster --recursive --exclude "*" --include "*_0442*.tif"
+	aws s3 cp --no-sign-request s3://download.opencontent.netflix.com/Chimera/tif_DCI4k5994p/ chimera_coaster --recursive --exclude "*" --include "*_0443*.tif"
+fi
+
+
+if [ ! -d chimera_cars_srgb ]
+then
+        echo Downloading netflix cars.
+        aws s3 cp --no-sign-request s3://download.opencontent.netflix.com/Chimera/tif_DCI4k2398p/ chimera_cars --recursive --exclude "*" --include "*_025*.tif"
+        aws s3 cp --no-sign-request s3://download.opencontent.netflix.com/Chimera/tif_DCI4k2398p/ chimera_cars --recursive --exclude "*" --include "*_026*.tif"
+fi
+
+if [ ! -d chimera_fountains_srgb ]
+then
+        echo Downloading netflix fountains
+        aws s3 cp --no-sign-request s3://download.opencontent.netflix.com/Chimera/tif_DCI4k2398p/ chimera_fountains --recursive --exclude "*" --include "*_054*.tif"
+        aws s3 cp --no-sign-request s3://download.opencontent.netflix.com/Chimera/tif_DCI4k2398p/ chimera_fountains --recursive --exclude "*" --include "*_055*.tif"
+fi
+
+# I think the file labeling is actually incorrect, this media is actually pretty close to gamma2.2 or 2.4 its not HDR at all. 
+# The associated mov file has no HDR metadata associated with it. So I'm going to assume no color space conversion is necessary.
+
+if [ ! -d chimera_wind_srgb ]
+then
+    mkdir chimera_wind_srgb
+	echo Building chimera_wind png
+	oiiotool -v --framepadding 5 --frames 79000-79199 -i chimera_wind/Chimera_DCI4k5994p_HDR_P3PQ_@@@@@.tif --resize 2048x1080 -dither -o chimera_wind_srgb/chimera_wind_srgb.#.png
+	rm -rf chimera_wind
+fi
+
+if [ ! -d chimera_coaster_srgb ]
+then
+    mkdir chimera_coaster_srgb
+	echo Building chimera_coaster_srgb png
+	oiiotool -v --framepadding 5 --frames 44200-44399 -i chimera_coaster/Chimera_DCI4k5994p_HDR_P3PQ_@@@@@.tif --resize 2048x1080 -dither -o chimera_coaster_srgb/chimera_coaster_srgb.#.png
+	#rm -rf chimera_coaster
+fi
+
+
+if [ ! -d chimera_cars_srgb ]
+then
+    mkdir chimera_cars_srgb
+	echo Building chimera_cars png
+	oiiotool -v --framepadding 5 --frames 2500-2699 -i chimera_cars/Chimera_DCI4k2398p_HDR_P3PQ_@@@@@.tif --resize 2048x1080 -dither -o chimera_cars_srgb/chimera_cars_srgb.#.png
+	rm -rf chimera_cars
+fi
+
+
+if [ ! -d chimera_fountains_srgb ]
+then
+    mkdir chimera_fountains_srgb
+	echo Building chimera_fountains png
+	oiiotool -v --framepadding 5 --frames 5400-5599 -i chimera_fountains/Chimera_DCI4k2398p_HDR_P3PQ_@@@@@.tif --resize 2048x1080 -dither -o chimera_fountains_srgb/chimera_fountains_srgb.#.png
+	rm -rf chimera_fountains
+fi
---
+++ enctests/sources/hdr_sources/download_media.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/bash -xv
+
+#downloading sparks
+
+# https://opencontent.netflix.com/
+# This first bit of media is really just for sparks, which hopefully should stress encoders.
+if [ ! -d sparks ]
+then
+	echo Downloading netflix sparks pt1.
+	aws s3 cp --no-sign-request s3://download.opencontent.netflix.com/sparks/aces_image_sequence_59_94_fps/ sparks --recursive --exclude "*" --include "SPARKS_ACES_061*.exr"
+	aws s3 cp --no-sign-request s3://download.opencontent.netflix.com/sparks/aces_image_sequence_59_94_fps/ sparks --recursive --exclude "*" --include "SPARKS_ACES_062*.exr"
+fi
+
+
+# https://opencontent.netflix.com/
+# This is a second extract from sparks, which is a great HDR test.
+if [ ! -d sparks2 ]
+then
+	echo Downloading netflix sparks pt2.
+	aws s3 cp --no-sign-request s3://download.opencontent.netflix.com/sparks/aces_image_sequence_59_94_fps/ sparks2 --recursive --exclude "*" --include "SPARKS_ACES_067*.exr"
+	aws s3 cp --no-sign-request s3://download.opencontent.netflix.com/sparks/aces_image_sequence_59_94_fps/ sparks2 --recursive --exclude "*" --include "SPARKS_ACES_068*.exr"
+fi
+
+if [ ! -d sparks_srgb ]
+then
+    mkdir sparks_srgb
+	echo Building sparks png
+	oiiotool -v --framepadding 5 --frames 6100-6299 sparks/SPARKS_ACES_#.exr --resize 1920x1014 \
+       --colorconvert linear srgb --dither -o sparks_srgb/sparks_srgb.#.png
+
+fi
+
+if [ ! -d sparks2_srgb ]
+then
+    mkdir sparks2_srgb
+	echo Building sparks2_srgb
+	oiiotool -v --framepadding 5 --frames 6700-6899 sparks2/SPARKS_ACES_#.exr --resize 1920x1014 \
+       --colorconvert acescg out_srgb --dither -o sparks2_srgb/sparks2_srgb.#.png
+
+fi
+
+if [ ! -d sparks2_hlg ]
+then
+    mkdir sparks2_hlg
+	echo Building sparks2_hlg
+    oiiotool -v --framepadding 5 --frames 6700-6899 sparks2/SPARKS_ACES_#.exr --resize 1920x1014 --colorconvert acescg out_rec2020hlg1000nits -d uint16 -o sparks2_hlg/sparks2_hlg.#.png
+fi
+
+ # Encoding test
+# ffmpeg  -sws_flags print_info+accurate_rnd+bitexact+full_chroma_int   -color_range pc   -color_trc arib-std-b67   -color_primaries bt2020   -colorspace bt2020nc   -pix_fmt rgb48be  -r 30 -start_number 6700 -i sparks2_hlg/sparks2_hlg.%05d.png   -c:v libx265   -color_range tv   -color_trc arib-std-b67   -color_primaries bt2020   -colorspace bt2020nc   -pix_fmt yuv444p10le   -sws_flags print_info+accurate_rnd+bitexact+full_chroma_int   -x265-params ‘colorprim=bt2020:transfer=arib-std-b67:colormatrix=bt2020nc:range=limited:master-display=G\(13250,34500\)B\(7500,3000\)R\(34000,16000\)WP\(15635,16450\)L\(10000000,1\):max-cll=1000,400’   sparks2_hlg_444.mov
+
+# ffmpeg  -sws_flags print_info+accurate_rnd+bitexact+full_chroma_int   -color_range pc   -color_trc arib-std-b67   -color_primaries bt2020   -colorspace bt2020nc   -pix_fmt rgb48be  -r 30 -start_number 6700 -i sparks2_hlg/sparks2_hlg.%05d.png   -c:v libx265   -color_range tv   -color_trc arib-std-b67   -color_primaries bt2020   -colorspace bt2020nc   -pix_fmt yuv420p10le   -sws_flags print_info+accurate_rnd+bitexact+full_chroma_int   -x265-params ‘colorprim=bt2020:transfer=arib-std-b67:colormatrix=bt2020nc:range=limited:master-display=G\(13250,34500\)B\(7500,3000\)R\(34000,16000\)WP\(15635,16450\)L\(10000000,1\):max-cll=1000,400’   sparks2_hlg_420.mov
+
+#ffmpeg -r 30 -start_number 6700 -i sparks2_srgb/sparks2_srgb.%05d.png -vf "scale=in_color_matrix=bt709:out_color_matrix=bt709" \
+#        -c:v libx264 -preset slower -pix_fmt yuv420p \
+#        -color_range 1 -colorspace 1 -color_primaries 1 -color_trc 13 sparks2_srgb_420_v2.mov
+
+# ffmpeg  -sws_flags print_info+accurate_rnd+bitexact+full_chroma_int   -color_range pc   -color_trc arib-std-b67   -color_primaries bt2020   -colorspace bt2020nc   -pix_fmt rgb48be  -r 30 -start_number 6700 -i sparks2_hlg/sparks2_hlg.%05d.png   -c:v libx265   -color_range tv   -color_trc arib-std-b67   -color_primaries bt2020   -colorspace bt2020nc   -pix_fmt yuv420p10le  -tag:v hvc1  -sws_flags print_info+accurate_rnd+bitexact+full_chroma_int   -x265-params ‘colorprim=bt2020:transfer=arib-std-b67:colormatrix=bt2020nc:range=limited:master-display=G\(13250,34500\)B\(7500,3000\)R\(34000,16000\)WP\(15635,16450\)L\(10000000,1\):max-cll=1000,400’   sparks2_hlg_420_v2.mov
