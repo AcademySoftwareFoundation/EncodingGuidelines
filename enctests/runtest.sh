9fe426bcd5d2ca22f1c5e89dfb8f1bef267d2fba
Sam.Richards@taurich.org <Sam.Richards@taurich.org>
2023-06-26 14:07:48 +0100

    Adding av1, dnxhd and hevc tests
    Signed-off-by: Sam.Richards@taurich.org <Sam.Richards@taurich.org>

--- enctests/runtest.sh
+++ enctests/runtest.sh
@@ -11,8 +11,11 @@ python3 -m testframework.main --source-folder sources/enc_sources --test-config 
 python3 -m testframework.otio2html --test-config test_configs/h264_crf_tests.yml --results h264-crf-results.otio
 
 python3 -m testframework.main --source-folder sources/enc_sources --test-config test_configs/color_tests.yml --output color-results.otio --encoded-folder color-encode
+python3 -m testframework.main --source-folder sources/enc_sources --test-config test_configs/hevc_color_tests.yml --output hevc-color-results.otio --encoded-folder hevc-color-encode
 
-#WIP
+python3 -m testframework.main --source-folder sources/enc_sources --test-config test_configs/av1_color_tests.yml --output av1-color-results.otio --encoded-folder av1-color-encode
+python3 -m testframework.main --source-folder sources/enc_sources --test-config test_configs/dnxhd_color_tests.yml --output dnxhd-color-results.otio --encoded-folder dnxhd-color-encode
+
 python3 -m testframework.generatetests
 
 python3 -m testframework.main --test-config test_configs/documentation_tests.yml --output doc-results.otio --encoded-folder docs-encode
